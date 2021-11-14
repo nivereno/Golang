cb5ac4e395937c49e99b5b745d56d78ba11bacbc
nivereno@gmail.com <nivereno@gmail.com>
2021-11-14 08:44:49 +0300

    pls

---
+++ GolangProject/Test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	//"handlers"
+	"github.com/nivereno/Go/tree/Augh/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	http.ListenAndServe(":9090", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nivereno/Go
+
+go 1.17
