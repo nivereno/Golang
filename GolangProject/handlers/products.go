46cfa2cd70d4483752405f04fddbda1f3bdb177b
nivereno@gmail.com <nivereno@gmail.com>
2021-11-15 09:48:17 +0300

    Added puts and stuff

--- GolangProject/handlers/products.go
+++ GolangProject/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/nivereno/Golang/GolangProject/data"
 )
@@ -25,6 +27,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		p.addProducts(rw, r)
 		return
+	}
+
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.l.Println("got id", id)
 	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
