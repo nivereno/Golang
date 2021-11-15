5ba6c5da71bdd66a074c92649b2577665ba76480
nivereno@gmail.com <nivereno@gmail.com>
2021-11-15 08:30:05 +0300

    Added converting to JSON and some formating

--- GolangProject/handlers/products.go
+++ GolangProject/handlers/products.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/nivereno/Golang/tree/main/data"
+	"github.com/nivereno/Golang/GolangProject/data"
 )
 
 type Products struct {
@@ -17,4 +17,10 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
 	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+
+	rw.Write(d)
 }
